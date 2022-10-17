dbb5c8256f2ef8c212c8f3b57c77b12e46434e8a
naman-gupta99 <kungfunaman@gmail.com>
2022-10-17 16:57:38 +0530

    Naman | Initial Commit

---
+++ script.js
@@ -0,0 +1,45 @@
+TweenLite.set(".center", {
+  xPercent: -50,
+  yPercent: -50,
+});
+
+TweenMax.to("#earth", 4, {
+  bezier: getBezier(250, 250, 250, 250, true),
+  repeat: -1,
+  ease: Linear.easeNone,
+});
+
+TweenMax.to("#saturn", 2, {
+  bezier: getBezier(250, 250, -200, -200, true),
+  repeat: -1,
+  ease: Linear.easeNone,
+});
+
+function getBezier(cx, cy, rx, ry, autoRotate) {
+  ry = ry || rx;
+
+  var k = 0.551915024494;
+  var x = k * rx;
+  var y = k * ry;
+
+  const values = [
+    { x: cx + rx, y: cy },
+    { x: cx + rx, y: cy + y },
+    { x: cx + x, y: cy + ry },
+    { x: cx, y: cy + ry },
+    { x: cx - x, y: cy + ry },
+    { x: cx - rx, y: cy + y },
+    { x: cx - rx, y: cy },
+    { x: cx - rx, y: cy - y },
+    { x: cx - x, y: cy - ry },
+    { x: cx, y: cy - ry },
+    { x: cx + x, y: cy - ry },
+    { x: cx + rx, y: cy - y },
+    { x: cx + rx, y: cy },
+  ];
+  return {
+    autoRotate: autoRotate || false,
+    type: "cubic",
+    values: values,
+  };
+}
